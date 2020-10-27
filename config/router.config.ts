29b78566c229e6a20b0c281a4a190600b934e3e3
李先森 <1440955662@qq.com>
2020-10-27 23:46:14 +0800

    404

--- config/router.config.ts
+++ config/router.config.ts
@@ -1,46 +1,55 @@
 module.exports = {
-    routes: [
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: '登录中心',
+      component: '@/pages/login',
+      exact: true,
+    },
+    {
+      path: '/admin',
+      name: '后台管理系统',
+      component: '@/layout/index',
+      // exact: true,
+      routes: [
         {
-            path: '/',
-            redirect: '/login',
+          // exact: true,
+          path: '/admin',
+          name: '首页',
+          component: '@/pages/home',
         },
         {
-            path: '/login',
-            name: '登录中心',
-            component: '@/pages/login',
-            exact: true,
+          // exact: true,
+          name: 'home',
+          routes: [
+            {
+              // exact: true,
+              path: '/admin/111111',
+              name: '111111',
+              component: '@/pages/home copy',
+            },
+            {
+              // exact: true,
+              path: '/admin/222222',
+              name: '222222',
+              component: '@/pages/home copy 2',
+            },
+            {
+              component: '@/pages/404',
+            },
+          ],
         },
         {
-            path: '/admin',
-            name: '后台管理系统',
-            component: '@/layout/index',
-            // exact: true,
-            routes: [
-                {
-                    // exact: true,
-                    path: '/admin',
-                    name: '首页',
-                    component: '@/pages/home',
-                },
-                {
-                    // exact: true,
-                    name: 'home',
-                    routes: [
-                        {
-                            // exact: true,
-                            path: '/admin/111111',
-                            name: '111111',
-                            component: '@/pages/home copy'
-                        },
-                        {
-                            // exact: true,
-                            path: '/admin/222222',
-                            name: '222222',
-                            component: '@/pages/home copy 2',
-                        },
-                    ],
-                },
-            ],
+          component: '@/pages/404',
         },
-    ],
+      ],
+    },
+    {
+      component: '@/pages/404',
+    },
+  ],
 };
---
+++ src/pages/404/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Index: FC = () => {
+  return <div>这个是404页面</div>;
+};
+
+export default Index;
